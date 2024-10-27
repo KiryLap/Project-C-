62b26b1ff8bfc75de03f4cc7bd104b3de92c5c4d
Jessk1y <zaha-05@mail.ru>
2024-10-27 19:41:43 +0300

    Код
    Написана первая версия кода с Алгоритмом Бойера-Мура

--- Sannikov/functions4_by_Sannikov.cpp
+++ Sannikov/functions4_by_Sannikov.cpp
@@ -3,7 +3,42 @@
 
 using namespace std;
 
-// Здесь прописываем, что будет выполнять функция, которая объявлена в .h
-// Пример: int fun4(int a, int b) {
-            //return a + b;
-        //}
+BoyerMoore::BoyerMoore(const std::string& pattern) : pattern(pattern) {
+    // Инициализация таблицы последнего появления
+    for (size_t i = 0; i < pattern.size(); ++i) {
+        last_occurrence[pattern[i]] = i;
+    }
+
+    // Инициализация таблицы переходов
+    jump_table.resize(256, pattern.size());
+    for (size_t i = 0; i < pattern.size() - 1; ++i) {
+        jump_table[(unsigned char)pattern[i]] = pattern.size() - i - 1;
+    }
+}
+
+int BoyerMoore::search(const std::string& text) {
+    int j = 0;
+    for (size_t i = pattern.size() - 1; i < text.size(); ) {
+        if (text[i] == pattern[j]) {
+            if (j == 0) {
+                return i - pattern.size() + 1;
+            }
+            --j;
+            --i;
+        }
+        else {
+            // Используем таблицу переходов
+            int jump = jump_table[(unsigned char)text[i]];
+            // Используем таблицу последнего появления
+            if (last_occurrence.count(text[i])) {
+                int last_index = last_occurrence[text[i]];
+                if (last_index < jump) {
+                    jump = last_index;
+                }
+            }
+            i += jump;
+            j = pattern.size() - 1;
+        }
+    }
+    return -1;
+}
--- Sannikov/functions4_by_Sannikov.h
+++ Sannikov/functions4_by_Sannikov.h
@@ -1,10 +1,20 @@
 #ifndef FUNCTIONS4_H
 #define FUNCTIONS4_H
 
-//Если нужно прописать дополнительные include, то их пишем здесь
-// Пример: #include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <iostream>
 
-//Здесь прописываешь названия функций с передаваемыми переменными и т.д
-// Пример: int fun4(int a, int b);
+class BoyerMoore {
+public:
+    BoyerMoore(const std::string& pattern);
+    int search(const std::string& text);
+
+private:
+    std::unordered_map<char, int> last_occurrence;
+    std::vector<int> jump_table;
+    std::string pattern;
+};
 
 #endif 
